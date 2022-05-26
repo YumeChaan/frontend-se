17f80970f7b1b34439c781f13bfc02b68965df40
Piyumi Chan Mahaarachchi <76487449+YumeChaan@users.noreply.github.com>
2022-05-26 16:13:03 +0530

    login css updated

--- src/Pages/LoginPage/index.jsx
+++ src/Pages/LoginPage/index.jsx
@@ -1,5 +1,6 @@
 import React, {useState} from "react";
-// import styles from './login.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import styles from './login.module.css'
 import login from './login2.jpg'
 import logo from './logo.png'
 
@@ -51,25 +52,25 @@ function Login() {
 
     const renderErrorMessage = (name) =>
     name === errorMessages.name && (
-      <div className="error">{errorMessages.message}</div>
+      <div className={`${styles["error"]}`}>{errorMessages.message}</div>
     );
 
     const renderForm = (
-        <div className="form-log">
-            <form action="#!" className="login-form" onSubmit={handleSubmit}>
-                <div className="form-group align-items-center">
-                    <label for="username" className="sr-only text-center">Email</label>
-                    <input type="username" name="uname" id="username" className="form-control text-center" placeholder="Username" required/>
+        <div className={`form-log`}>
+            <form action="#!" className={`${styles["login-form"]}`} onSubmit={handleSubmit}>
+                <div className={`form-group align-items-center`}>
+                    <label for="username" className={`sr-only`}>Email</label>
+                    <input type="username" name="uname" id="username" className={`form-control text-center ${styles["f-cont"]}`} placeholder="Username" required/>
                     {renderErrorMessage("uname")}
                 </div>
 
                 <div class="form-group mb-4">
-                    <label for="password" className="sr-only">Password</label>
-                    <input type="password" name="pass" id="password" className="form-control text-center" placeholder="***********" required/>
+                    <label for="password" className={`sr-only`}>Password</label>
+                    <input type="password" name="pass" id="password" className={`form-control text-center ${styles["f-cont"]}`} placeholder="***********" required/>
                     {renderErrorMessage("pass")}
                 </div>
 
-                <input name="login" id="login" className="btn btn-block login-btn mb-4" type="submit" value="Login" />
+                <input name="login" id="login" className={`btn btn-block mb-4 ${styles["login-btn"]}`} type="submit" value="Login" />
             </form>
         </div>
         
@@ -77,21 +78,21 @@ function Login() {
     
     return (
         <div>
-            <main className="d-flex align-items-center min-vh-100 py-3 py-md-0">
-                <div className="container">
-                <div className="card login-card">
-                    <div className="row no-gutters">
-                    <div className="col-lg-6 col-md-5">
-                        <img src={login} alt="login" className="login-card-img" /> 
+            <main className={`d-flex align-items-center min-vh-100 py-3 py-md-0`}>
+                <div className={`container ${styles["login-container"]}`}>
+                <div className={`card ${styles["login-card"]}`}>
+                    <div className={`row no-gutters`}>
+                    <div className={`col-lg-6 col-md-5`}>
+                        <img src={login} alt="login" className={`${styles["login-card-img"]}`} /> 
                     </div>
-                    <div className="col-lg-6 col-md-7 ">
-                        <div className="card-body text-center">
-                            <div className="brand-wrapper">
-                                <img src={logo} alt="logo" class="logo" />
+                    <div className={`col-lg-6 col-md-7`}>
+                        <div className={`card-body text-center ${styles["c-body"]}`}>
+                            <div className={`${styles["brand-wrapper"]}`}>
+                                <img src={logo} alt="logo" className={`${styles["logo"]}`} />
                             </div>
-                            <p className="login-card-description">Sign into your account</p>
+                            <p className={`${styles["login-card-description"]}`}>Sign into your account</p>
                             {isSubmitted ? <div></div> : renderForm}
-                            <p className="login-card-footer-text">Don't have an account? <a href="#!" className="text-reset">Register here</a></p>
+                            <p className={`${styles["login-card-footer-text"]}`}>Don't have an account? <a href="#!" className={`text-reset`}>Register here</a></p>
                         </div>
                     </div>
                     </div>
