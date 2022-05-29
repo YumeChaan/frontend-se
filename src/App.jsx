ace3e1693992f27cc13c2fcda99a679f35ed1cad
SumeelaMadusankha <hasumeela@gmail.com>
2022-05-29 19:11:58 +0530

    servises added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import AppRouter from './Router';
-
+import RegisterNavBar from './Components/RegisterNavBar';
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
 function App() {
   return (
+    <React.Fragment>
+      <ToastContainer/>
+      <RegisterNavBar/>
     <AppRouter />
+    </React.Fragment>
+      
+   
+    
   );
 }
 
